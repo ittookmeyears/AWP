0f48357b35b414ef8899b01fd5a5d9b6bf4f9ce4
unknown <kirillk@ETY-B303-19.edu.wks.metropolia.fi>
2015-01-12 12:31:04 +0200

    first commit

---
+++ week1/get.php
@@ -0,0 +1,54 @@
+﻿<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+</head>
+<body>
+
+<div id="accordion">
+<?php
+    $host = 'mysql.metropolia.fi';
+    $dbname = 'kirillk'; // your username
+    $user = 'kirillk'; // your username
+    $pass = 'vepeyue'; // your database password
+    
+    try {
+            $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+            $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            $DBH->query("SET NAMES 'UTF8';");
+        
+        } catch(PDOException $e) {
+            echo "Could not connect to database.";
+            file_put_contents('log.txt', $e->getMessage(), FILE_APPEND);
+        }
+
+try {
+    $eventList = array();
+    $sql = "SELECT * FROM calendar";
+    $STH = $DBH->query($sql);
+    $STH->setFetchMode(PDO::FETCH_OBJ);
+    while ($row = $STH->fetch()){
+    	echo '<h3>'.$row->eDate.'<br><strong>'.$row->eName.'</strong></h3><div><i>'.$row->eDescription.'</i><br><br>With:<br>'.$row->pEmail.'<br>'.$row->pPhone.'</div>';
+    }
+ } catch (PDOException $e) {
+	echo 'Something went wrong';
+	file_put_contents('log.txt', $e->getMessage()."\n\r", FILE_APPEND); // remember to set the permissions so that log.txt can be created
+}      
+
+?>
+</div>
+<script>
+// TODO: use jQuery UI to make an accordion from <div id="accordion">. 
+// Set 'collapsible' property to 'true' and 'active' property to 'none'
+$(function() {
+$( "#accordion" ).accordion({
+collapsible: true,
+active: "none"
+});
+});
+</script>
+
+</body>
+</html>
